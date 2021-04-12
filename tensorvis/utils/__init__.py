15588ed94753240cc9eb05bf0ed991bd281794df
Nikolas Pitsillos <npitsillos@gmail.com>
2021-04-12 11:57:44 +0100

    file formatting

--- tensorvis/utils/__init__.py
+++ tensorvis/utils/__init__.py
@@ -1,6 +1,6 @@
-from tensorvis.utils.utils import draw_line, separate_exps
+from tensorvis.utils.utils import draw_line, draw_scatter, separate_exps
 
-DRAW_FN_MAP = {"line": draw_line}
+DRAW_FN_MAP = {"line": draw_line, "scatter": draw_scatter}
 
 __all__ = [
     "separate_exps",
